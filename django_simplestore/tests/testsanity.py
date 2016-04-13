1402711910c771eae1caf586c37dea3c65ae95f6
joran <joran@decagon.com>
2016-04-12 23:00:41 -0700

    housekeeping

---
+++ django_simplestore/tests/testsanity.py
@@ -0,0 +1,12 @@
+import os
+
+from django.test import TestCase
+
+from django_simplestore.constants import ONE_HOUR
+class SanityTests(TestCase):
+    def test_sanity(self):
+        self.assertIn(os.environ['DJANGO_SETTINGS_MODULE'],['django_simplestore.settings','django_simplestore.tests.test_settings'])
+    def test_sanity0(self):
+        self.assertEqual(1+1,2)
+    def test_hour_const(self):
+        self.assertEqual(ONE_HOUR,3600)
