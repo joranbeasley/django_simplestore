888af67f6c66c0894ac8de84edb3e8a8ee2079f1
joran <joran@decagon.com>
2016-04-12 19:57:08 -0700

    added a testrunner and fixed some tests to run on travis CI ... also added missing folders to git

--- django_simplestore/tests/test_Sanity.py
+++ django_simplestore/tests/test_Sanity.py
@@ -5,6 +5,6 @@ from django.test import TestCase
 
 class SanityTests(TestCase):
     def test_sanity(self):
-        self.assertEqual(os.environ['DJANGO_SETTINGS_MODULE'],'django_simplestore.settings')
+        self.assertIn(os.environ['DJANGO_SETTINGS_MODULE'],['django_simplestore.settings','django_simplestore.tests.test_settings'])
     def test_sanity0(self):
         self.assertEqual(1+1,2)
--- django_simplestore/tests/unitTests/testModels.py
+++ django_simplestore/tests/unitTests/testModels.py
@@ -5,8 +5,6 @@ from django.test import TestCase
 from django_simplestore.models import Product,Page
 
 class ProductTests(TestCase):
-    def test_sanity(self):
-        self.assertEqual(os.environ['DJANGO_SETTINGS_MODULE'],'django_simplestore.settings')
     def test_product_unicode(self):
         product = Product(product_name="Test Product1",product_cost=2.25,product_description="Test Description",product_ingredients="")
         self.assertEqual(unicode(product),"<Product name='Test Product1' cost='$2.25'>")
